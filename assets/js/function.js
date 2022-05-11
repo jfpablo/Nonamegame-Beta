9e95c56e8f211455245feb264f8c71e11be9e776
Bananoob <mathis.jardon@gmail.com>
2022-05-11 21:52:11 +0200

    second commit

---
+++ assets/js/function.js
@@ -0,0 +1,7 @@
+function fermerlescookies() {
+    idcookies.style.display = "none";
+}
+function functiontogfullscreen() {
+    idtitlepage.style.display ="none"
+    idcontainer.style.height ="922px"
+}
