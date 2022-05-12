061c40614d7641f6627f27c4829fa60a2028c7bb
Bananoob <mathis.jardon@gmail.com>
2022-05-12 07:36:20 +0200

    thirth commit

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,4 +1,7 @@
-function fermerlescookies() {
+function nocookies() {
+    idcookies.style.display = "none";
+}
+function yescookies() {
     idcookies.style.display = "none";
 }
 function functiontogfullscreen() {
